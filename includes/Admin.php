6b0fc14839dd20734cbb6ca47c7c1a569bda7f52
Mak Alamin <makalamin012@gmail.com>
2021-11-19 04:00:19 +0600

    Edited something

--- includes/Admin.php
+++ includes/Admin.php
@@ -1,24 +1,24 @@
-<?php 
+<?php
 
 namespace Wedevs\Academy;
 
 /**
  * Admin handler class
  */
-class Admin  
+class Admin
 {
-    function __construct()
+    public function __construct()
     {
         $addressbook = new Admin\Addressbook();
 
         $this->dispatch_actions($addressbook);
-        
+
         new Admin\Menu($addressbook);
     }
 
     public function dispatch_actions($addressbook)
     {
-        add_action('admin_init', [ $addressbook, 'form_handler']);
-        add_action('admin_post_wd-ac-delete-address', [ $addressbook, 'delete_address']);
+        add_action('admin_init', [$addressbook, 'form_handler']);
+        add_action('admin_post_wd-ac-delete-address', [$addressbook, 'delete_address']);
     }
 }
--- includes/Admin/Address_List.php
+++ includes/Admin/Address_List.php
@@ -2,22 +2,23 @@
 
 namespace Wedevs\Academy\Admin;
 
-if ( ! class_exists( 'WP_List_Table' ) ) {
+if (!class_exists('WP_List_Table')) {
     require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
 }
 
 /**
  * Address List Class
  */
-class Address_List extends \WP_List_Table 
+class Address_List extends \WP_List_Table
+
 {
-    function construct()
+    public function construct()
     {
-        parent::__construct( [
+        parent::__construct([
             'singular' => 'contact',
-            'plural'   => 'contacts',
-            'ajax'     => false
-        ]);    
+            'plural' => 'contacts',
+            'ajax' => false,
+        ]);
     }
 
     /**
@@ -25,8 +26,9 @@ class Address_List extends \WP_List_Table
      *
      * @return void
      */
-    function no_items() {
-        _e( 'No address found', 'wedevs-academy' );
+    public function no_items()
+    {
+        _e('No address found', 'wedevs-academy');
     }
 
     /**
@@ -34,45 +36,45 @@ class Address_List extends \WP_List_Table
      *
      * @return array
      */
-    public function get_columns() {
+    public function get_columns()
+    {
         return [
-            'cb'         => '<input type="checkbox" />',
-            'name'       => __( 'Name', 'wedevs-academy' ),
-            'address'    => __( 'Address', 'wedevs-academy' ),
-            'phone'      => __( 'Phone', 'wedevs-academy' ),
-            'created_at' => __( 'Date', 'wedevs-academy' ),
+            'cb' => '<input type="checkbox" />',
+            'name' => __('Name', 'wedevs-academy'),
+            'address' => __('Address', 'wedevs-academy'),
+            'phone' => __('Phone', 'wedevs-academy'),
+            'created_at' => __('Date', 'wedevs-academy'),
         ];
     }
-
 
     /**
      * Get sortable columns
      *
      * @return array
      */
-    function get_sortable_columns() {
+    public function get_sortable_columns()
+    {
         $sortable_columns = [
-            'name'       => [ 'name', true ],
-            'created_at' => [ 'created_at', true ],
+            'name' => ['name', true],
+            'created_at' => ['created_at', true],
         ];
 
         return $sortable_columns;
     }
-
 
     /**
      * Set the bulk actions
      *
      * @return array
      */
-    function get_bulk_actions() {
+    public function get_bulk_actions()
+    {
         $actions = array(
-            'trash'  => __( 'Move to Trash', 'wedevs-academy' ),
+            'delete' => __('Delete', 'wedevs-academy'),
         );
 
         return $actions;
     }
-
 
     /**
      * Default column values
@@ -82,15 +84,16 @@ class Address_List extends \WP_List_Table
      *
      * @return string
      */
-    protected function column_default( $item, $column_name ) {
+    protected function column_default($item, $column_name)
+    {
 
-        switch ( $column_name ) {
+        switch ($column_name) {
 
             case 'created_at':
-                return wp_date( get_option( 'date_format' ), strtotime( $item->created_at ) );
+                return wp_date(get_option('date_format'), strtotime($item->created_at));
 
             default:
-                return isset( $item->$column_name ) ? $item->$column_name : '';
+                return isset($item->$column_name) ? $item->$column_name : '';
         }
     }
 
@@ -101,32 +104,32 @@ class Address_List extends \WP_List_Table
      *
      * @return string
      */
-    protected function column_cb( $item ) {
+    protected function column_cb($item)
+    {
         return sprintf(
             '<input type="checkbox" name="address_id[]" value="%d" />', $item->id
         );
     }
 
-
-     /**
+    /**
      * Render the "name" column
      *
      * @param  object $item
      *
      * @return string
      */
-    public function column_name( $item ) {
+    public function column_name($item)
+    {
         $actions = [];
 
-        $actions['edit']   = sprintf( '<a href="%s" title="%s">%s</a>', admin_url( 'admin.php?page=wedevs-academy&action=edit&id=' . $item->id ), $item->id, __( 'Edit', 'wedevs-academy' ), __( 'Edit', 'wedevs-academy' ) );
-    
-        $actions['delete'] = sprintf( '<a href="%s" class="submitdelete" onclick="return confirm(\'Are you sure?\');" title="%s">%s</a>', wp_nonce_url( admin_url( 'admin-post.php?action=wd-ac-delete-address&id=' . $item->id ), 'wd-ac-delete-address' ), $item->id, __( 'Delete', 'wedevs-academy' ), __( 'Delete', 'wedevs-academy' ) );
+        $actions['edit'] = sprintf('<a href="%s" title="%s">%s</a>', admin_url('admin.php?page=wedevs-academy&action=edit&id=' . $item->id), $item->id, __('Edit', 'wedevs-academy'), __('Edit', 'wedevs-academy'));
+
+        $actions['delete'] = sprintf('<a href="%s" class="submitdelete" onclick="return confirm(\'Are you sure?\');" title="%s">%s</a>', wp_nonce_url(admin_url('admin-post.php?action=wd-ac-delete-address&id=' . $item->id), 'wd-ac-delete-address'), $item->id, __('Delete', 'wedevs-academy'), __('Delete', 'wedevs-academy'));
 
         return sprintf(
-            '<a href="%1$s"><strong>%2$s</strong></a> %3$s', admin_url( 'admin.php?page=wedevs-academy&action=view&id' . $item->id ), $item->name, $this->row_actions( $actions )
+            '<a href="%1$s"><strong>%2$s</strong></a> %3$s', admin_url('admin.php?page=wedevs-academy&action=edit&id' . $item->id), $item->name, $this->row_actions($actions)
         );
     }
-  
 
     /**
      * Prepare the address items
@@ -135,31 +138,31 @@ class Address_List extends \WP_List_Table
      */
     public function prepare_items()
     {
-        $column   = $this->get_columns();
-        $hidden   = [];
+        $column = $this->get_columns();
+        $hidden = [];
         $sortable = $this->get_sortable_columns();
 
-        $this->_column_headers = [ $column, $hidden, $sortable ];
+        $this->_column_headers = [$column, $hidden, $sortable];
 
-        $per_page     = 4;
+        $per_page = 2;
         $current_page = $this->get_pagenum();
-        $offset       = ( $current_page - 1 ) * $per_page;
+        $offset = ($current_page - 1) * $per_page;
 
         $args = [
             'number' => $per_page,
             'offset' => $offset,
         ];
 
-        if ( isset( $_REQUEST['orderby'] ) && isset( $_REQUEST['order'] ) ) {
+        if (isset($_REQUEST['orderby']) && isset($_REQUEST['order'])) {
             $args['orderby'] = $_REQUEST['orderby'];
-            $args['order']   = $_REQUEST['order'] ;
+            $args['order'] = $_REQUEST['order'];
         }
 
         $this->items = wd_ac_get_addresses($args);
 
         $this->set_pagination_args([
             'total_items' => wd_ac_address_count(),
-            'per_page'    => $per_page
+            'per_page' => $per_page,
         ]);
     }
 }
